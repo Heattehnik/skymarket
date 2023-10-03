db0d22c3e7fe1753c9f68d5457f5b244c457b660
roman <roman.ecm@mail.ru>
2023-10-03 14:40:28 +0500

    fix: Comment creation on progress

--- skymarket/ads/urls.py
+++ skymarket/ads/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from ads.apps import SalesConfig
-from ads.views import AdViewSet, UserAdsListView, AdCommentsView, CreateCommentView
+from ads.views import AdViewSet, UserAdsListView, AdCommentsView  # CreateCommentView
 
 ad = DefaultRouter()
 ad.register(r"ads", AdViewSet, basename="ads")
@@ -11,6 +11,6 @@ app_name = SalesConfig.name
 
 urlpatterns = [
     path('ads/me/', UserAdsListView.as_view(), name='me_ads'),
-    path('ads/<int:ad_id>/comments/', AdCommentsView.as_view(), name='ad_comments'),
-    path('ads/<int:ad_pk>/comments/', CreateCommentView.as_view(), name='create_comment'),
+    path('ads/<int:ad_id>/comments/', AdCommentsView.as_view({'get': 'list'}), name='ad_comments'),
+    path('ads/<int:ad_id>/comments/', AdCommentsView.as_view({'post': 'create'}), name='create_comment'),
 ] + ad.urls
--- skymarket/ads/views.py
+++ skymarket/ads/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
 from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+
 from ads.models import Ad, Comment
 from ads.paginators import AdsPaginator, CommentsPaginator
 from ads.serializers import CommentSerializer, AdSerializer, AdDetailSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 # TODO view функции. Предлагаем Вам следующую структуру - но Вы всегда можете использовать свою
@@ -47,32 +51,57 @@ class UserAdsListView(ListAPIView):
         return queryset
 
 
-class AdCommentsView(RetrieveAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    lookup_field = 'ad_id'  # Set the lookup field to 'ad_id'
+class AdCommentsView(ViewSet):
+    permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        # The 'ad_id' parameter is now used as the lookup field
+    @action(methods=['get'], detail=True)
+    def list(self, request, ad_id=None):
+        # Обработка GET-запросов
+
+        # Извлечение ad_id из URL и использование его для получения комментариев
         ad_id = self.kwargs.get('ad_id')
         queryset = Comment.objects.filter(ad_id=ad_id)
-        return queryset
+
+        # Вы можете добавить дополнительную логику здесь, если это необходимо
+
+        # Создание сериализатора для комментариев и сериализация данных
+        serializer = CommentSerializer(queryset, many=True)
+
+        # Возврат сериализованных данных в HTTP-ответе
+        return Response(serializer.data)
+
+    @action(methods=['post'], detail=True)
+    def create(self, request, ad_id=None):
+        # Получите текущего пользователя (автора комментария)
+        author = request.user
+
+        # Извлечь данные из запроса
+        comment_data = request.data
+
+        # Создать новый комментарий с ad_id из URL, данными из запроса и автором
+        new_comment = Comment.objects.create(ad_id=ad_id, author=author, **comment_data)
+
+        # Вернуть HTTP-ответ с информацией о созданном комментарии
+        serializer = CommentSerializer(new_comment)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     pagination_class = CommentsPaginator
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
 
 
-class CreateCommentView(CreateAPIView):
-    serializer_class = CommentSerializer
-
-    def perform_create(self, serializer):
-        # Получите текущего пользователя (автора объявления)
-        author = self.request.user
-
-        # Сохраните объект объявления с указанным автором
-        serializer.save(author=author)
+# class CreateCommentView(CreateAPIView):
+#     permission_classes = [IsAuthenticated]
+#     serializer_class = CommentSerializer
+#
+#     def perform_create(self, serializer):
+#         # Получите текущего пользователя (автора объявления)
+#         author = self.request.user
+#
+#         # Сохраните объект объявления с указанным автором
+#         serializer.save(author=author)
 
 
